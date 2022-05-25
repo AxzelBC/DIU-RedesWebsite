d1315b370f68a5d7345f7b9c99de2822531c7a57
AxzelBC <Alejo13580@gmail.com>
2022-05-24 20:23:56 -0500

    Cleaning proyect. Creaction route const

---
+++ frontend/src/routes.js
@@ -0,0 +1,22 @@
+const routes = [
+    {
+        name: "Inicio",
+        key: "inicio",
+        route: "/",
+        component: <></>,
+    },
+    {
+        name: "Quienes somos",
+        key: "quienes-somos",
+        route: "/quienes-somos",
+        component: <></>,
+    },
+    {
+        name: "Que Nos Interesa",
+        key: "que-nos-interesa",
+        route: "/que-nos-interesa",
+        component: <></>,
+    }
+]
+
+export default routes;
