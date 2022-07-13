1bfad181c30896d2827c777eab4e9908ac02a5ea
ruzbellit.romero <ruzbellit.romero@correounivalle.edu.co>
2022-07-13 17:01:45 -0500

    Se agrega front de retroalimentacion e historial

--- frontend/src/pages/Admin/HistoryPC.js
+++ frontend/src/pages/Admin/HistoryPC.js
@@ -2,10 +2,43 @@ import React from "react";
 import Box from "@mui/material/Box";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+import DataTable from "examples/Tables/DataTable";
+
+import historyPcTableData from "pages/Admin/data/historyPcTableData";
+
 function HistoryPC() {
-    return(
+    const { columns, rows } = historyPcTableData();
+    return (
         <DashboardLayout>
-            Ver historial de reserva de equipos
+        <MDTypography display="block"  fontWeight="medium">
+        <h2>Historial reserva de equipos</h2>
+        <h5>Listado de las reservas de equipos del ultimo mes</h5>
+        </MDTypography>
+          <MDBox pt={3} pb={3}>
+            <Grid container spacing={6}>
+              <Grid item xs={12}>
+                <Card>
+                  <MDBox pt={3}>
+                    <DataTable
+                      table={{ columns, rows }}
+                      isSorted={false}
+                      entriesPerPage={false}
+                      showTotalEntries={false}
+                      noEndBorder
+                    />
+                  </MDBox>
+                </Card>
+              </Grid>
+            </Grid>
+          </MDBox>
         </DashboardLayout>
     )
 }
--- frontend/src/pages/Admin/ViewFeed.js
+++ frontend/src/pages/Admin/ViewFeed.js
@@ -2,10 +2,43 @@ import React from "react";
 import Box from "@mui/material/Box";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+import DataTable from "examples/Tables/DataTable";
+
+import viewFeedTableData from "pages/Admin/data/viewFeedTableData";
+
 function ViewFeed() {
+    const { columns, rows } = viewFeedTableData();
     return(
         <DashboardLayout>
-            Consultar retroalimentaciones
+        <MDTypography display="block"  fontWeight="medium">
+        <h2>Retroalimentaciones</h2>
+        <h5>Opiniones</h5>
+        </MDTypography>
+          <MDBox pt={3} pb={3}>
+            <Grid container spacing={6}>
+              <Grid item xs={12}>
+                <Card>
+                  <MDBox pt={3}>
+                    <DataTable
+                      table={{ columns, rows }}
+                      isSorted={false}
+                      entriesPerPage={false}
+                      showTotalEntries={false}
+                      noEndBorder
+                    />
+                  </MDBox>
+                </Card>
+              </Grid>
+            </Grid>
+          </MDBox>
         </DashboardLayout>
     )
 }
