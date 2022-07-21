c177542bd095dc3768df4615c897bfa30d4c9296
AxzelBC <alejo13580@gmail.com>
2022-07-20 21:24:37 -0500

    Cambios de implementación Se cambian los archivos principales de .JS a .JSX Se agrega la ruta de "Unirme"

---
+++ frontend/src/pages/LandingPages/Servicios/Reservas.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import MKBox from "components/MKBox";
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+import DefaultFooter from "examples/Footers/DefaultFooter";
+
+import SignIn from "layouts/pages/authentication/sign-in";
+
+// Routes
+import routes from "routes/pages.routes";
+import footerRoutes from "routes/footer.routes";
+
+function Reservas(){
+    return(
+        <>
+            <MKBox variant="gradient" bgColor="error" shadow="sm" py={0.25}>
+              <DefaultNavbar
+                routes={routes}
+                action={{
+                  type: "internal",
+                  route: "/login",
+                  component: <SignIn/>,
+                  label: "Integrantes",
+                  color: "default",
+                }}
+                sticky
+                transparent
+                relative
+                light
+                center
+              />
+            </MKBox>
+
+            <MKBox>
+                Reservas
+            </MKBox>
+
+            <MKBox pt={6} px={1} mt={6}>
+              <DefaultFooter content={footerRoutes} />
+            </MKBox>
+        </>
+    )
+}
+
+export default Reservas;
