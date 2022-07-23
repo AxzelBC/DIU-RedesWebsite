aa0434c04cfc9e8c92d480af7883bcf2ceffe1ba
AxzelBC <alejo13580@gmail.com>
2022-07-23 10:36:44 -0500

    Pruebas. Conexion a Articles. Conexión por Axios.

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,5 +2,9 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: 'https://lascilab-backend.herokuapp.com/',
-    withCredentials: false
+    withCredentials: false,
+    headers:{
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'localhost:3000'
+    },
 });
